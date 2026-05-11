0a97aff91ae36f336a1a5d6592a6c3539bec0cf2
SAMEBEST\S2309001 <claudia_laio1202@gmail.com>
2026-05-11 18:47:32 +0800

    pages ai100 修改

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import '@scss/styles.scss'
+// import '@scss/styles.scss'
 
 const app = createApp(App)
 
